ff17efe4e0725d54ab16742fb434c1560a37778b
don336 <smgmartinez92@gmail.com>
2023-11-28 11:58:19 +0300

    updated App.js

--- src/app.js
+++ src/app.js
@@ -5,7 +5,14 @@ import { errors } from 'celebrate';
 import routes from './routes/index';
 connect();
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      'https://concoe-coffee-and-foods-production.up.railway.app',
+      'https://personal-server-3fvv.onrender.com',
+    ],
+  })
+);
 app.use(express.json());
 app.use(errors());
 app.use(routes);
